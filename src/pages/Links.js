9db4e60fe28e2c8a7c2bf8608f5d040ab88f2b82
Michal Lapacz <michal.lapacz@ericsson.com>
2022-09-25 09:51:23 +0200

    loader, stubbed action bar icons, labels and tags

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -4,9 +4,13 @@
  */
 import './Links.css';
 import React, { useState, useEffect, useCallback } from 'react'
+import Chip from '@mui/material/Chip';
 import Card from '@mui/material/Card';
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import LinearProgress from '@mui/material/LinearProgress';
 import { styled } from "@mui/material/styles";
 import { firebaseService } from '../FirebaseService';
 
@@ -21,25 +25,36 @@ const CardContentCustomPadding = styled(CardContent)(`
 //     <div className="link">{props.name} <a href={props.url} {...(props.newTab && { target: '_blank', rel: 'noreferrer' })}>{props.url}</a></div>
 // );
 const Link = ({ link }) => (
-    <Card sx={{ display: 'inline-block', width: '100%' }}>
+    <Card className='card'>
         <CardContentCustomPadding>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom variant="caption">
+            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom variant="caption">
                 {link.name}
             </Typography>
-            {link.description && <Typography variant="subtitle1">
+            {link.description && <Typography variant="subtitle2">
                 {link.description}
             </Typography>}
+            {link.tags && <div>
+                Tags: {link.tags.map(tag => (<Chip key={tag} sx={{ p: '0', m: '0 2px' }} label={tag} size="small" />))}
+            </div>}
+            {link.keywords && <div>
+                Keywords: {link.keywords.map(keyword => (<Chip key={keyword} sx={{ p: '0', m: '0 2px' }} label={keyword} size="small" />))}
+            </div>}
             {link.url && <Typography variant="body2">
-                <a href={link.url} {...(link.newTab && { target: '_blank', rel: 'noreferrer' })}>{link.url}</a>
+                <a key={link.url} href={link.url} {...(link.newTab && { target: '_blank', rel: 'noreferrer' })}>{link.url}</a>
             </Typography>}
         </CardContentCustomPadding>
+        <div className="cardActionBar">
+            <IconButton aria-label="bookmark" variant="plain" size="sm">
+                <MoreVertIcon />
+            </IconButton>
+        </div>
     </Card>
 );
 Link.propTypes = {};
 
 const Links = () => {
 
-    const [linksList, setLinksList] = useState([]);
+    const [linksList, setLinksList] = useState(null);
 
     const getLinks = useCallback(async () => setLinksList(await firebaseService.getDocs("links")), []);
 
@@ -48,9 +63,12 @@ const Links = () => {
     }, [getLinks]);
 
     return (
-        <div className='links'>
-            {linksList.map(link => <Link key={link.url} link={link} name={link.name} url={link.url} newTab={link.newTab} />)}
-        </div>
+        <>
+            {!linksList && <div className="loadingWrapper"><LinearProgress /><div>Loading links...</div></div>}
+            <div className='links'>
+                {linksList && linksList.map(link => <Link key={link.url} link={link} name={link.name} url={link.url} newTab={link.newTab} />)}
+            </div>
+        </>
     )
 };
 
